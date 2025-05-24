6f58a6f934f3c3d06ca9ad4b48b707a60cf89377
Maxmiller Nunes <maxmillernunes11@gmail.com>
2025-05-23 23:43:01 -0300

    fix: :bug: change ports

--- src/env/index.ts
+++ src/env/index.ts
@@ -11,7 +11,7 @@ const envSchema = z.object({
   NODE_ENV: z.enum(['development', 'production', 'test']).default('production'),
   DATABASE_CLIENT: z.enum(['sqlite', 'pg']).default('sqlite'),
   DATABASE_URL: z.string(),
-  PORT: z.coerce.number().default(3000),
+  PORT: z.coerce.number().default(3333),
 })
 
 const _env = envSchema.safeParse(process.env)
--- src/server.ts
+++ src/server.ts
@@ -6,5 +6,5 @@ app
     port: env.PORT,
   })
   .then(() => {
-    console.log('Server is running on http://localhost:3333')
+    console.log('Server is running on port', env.PORT)
   })
